56883978d06bf94af99410d6498a4284844f0450
Eduardo Porto <daeddy64@gmail.com>
2022-05-21 10:19:48 +0900

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: "./index.html",
+      favicon: "./images/favicon.ico"
     })
   ],
   performance: {
